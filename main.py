65b889e8c0e22f0643dbeb0205b7cd05db6c3316
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-02-21 03:26:56 +0000

    feat: Added Flask web application with routes for home, unverified, verified, and search, added setup tasks, tasks, and devcontainer configuration for Python environment, added Dependabot configuration for weekly updates in Devcontainers ecosystem and added home.html template for the Flask application

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import flask
+from flask import Flask, render_template, request, redirect, url_for
 
+app = Flask(__name__,template_folder='templates')
+
+
+@app.route("/")
+def index():
+  return render_template("home.html")
+@app.route("/unverified")
+def unverified():
+  return render_template("unverified.html")
+@app.route("/verified")
+def verified():
+  return render_template("verified.html")
+@app.route("/search")
+if __name__ == "__main__":
+  app.run(debug = True,port=8080)
